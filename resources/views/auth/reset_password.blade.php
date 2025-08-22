44c9b6bc639ef6e2bc5ab9eebeba87f9ae47b30d
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-08-21 22:19:49 -0300

    resetar senha implementado

--- resources/views/auth/reset_password.blade.php
+++ resources/views/auth/reset_password.blade.php
@@ -1,1 +1,46 @@
-Resetar a senha
+@extends('layouts.auth')
+@section('body-class', 'register-page')
+@section('content')
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="../index2.html"><b>Admin</b>LTE</a>
+        </div>
+        <!-- /.register-logo -->
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="register-box-msg">Register a new membership</p>
+                <form action="{{ route('password.update') }}" method="post">
+                    @csrf
+
+                    <input type="hidden" name="token" value="{{ request()->token }}">
+
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}"/>
+                        <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" value="{{ old('password') }}"/>
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation" value="{{ old('password_confirmation') }}"/>
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                    </div>
+
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn-primary">Reset password</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
