3f4128be9638b590d67608eb2eb3827c62c526a6
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-22 09:05:35 -0300

    listando usuários, terminado

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,8 +6,10 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -29,16 +31,16 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Fortify::loginView(function() {
+        Fortify::loginView(function () {
             return view('auth.login');
         });
-        Fortify::registerView(function() {
+        Fortify::registerView(function () {
             return view('auth.register');
         });
-        Fortify::requestPasswordResetLinkView(function() {
+        Fortify::requestPasswordResetLinkView(function () {
             return view('auth.forgot_password');
         });
-        Fortify::resetPasswordView(function() {
+        Fortify::resetPasswordView(function () {
             return view('auth.reset_password');
         });
         Fortify::createUsersUsing(CreateNewUser::class);
@@ -47,7 +49,7 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
         RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . '|' . $request->ip());
 
             return Limit::perMinute(5)->by($throttleKey);
         });
