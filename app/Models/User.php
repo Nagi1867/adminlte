dc54456c7e62b4f9591becd0bfbdb1e9187ed4e9
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-08-25 10:24:01 -0300

    editar interesses

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
     public function profile() {
         return $this->hasOne(UserProfile::class);
     }
+
+    public function interests() {
+        return $this->hasMany(UserInterest::class);
+    }
 }
